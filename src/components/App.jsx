d1ea306f698af41a977aa5642126c8a61b1e0244
RuslanKabachok <ruslan.kabachok@gmail.com>
2022-03-16 19:12:01 +0200

    add Profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,19 @@
+import { Profile } from 'components/Profile/Profile';
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
+        // display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        // fontSize: 40,
+        // textTransform: 'uppercase',
         color: '#010101',
       }}
     >
-      React homework template
+      <Profile />
     </div>
   );
 };
